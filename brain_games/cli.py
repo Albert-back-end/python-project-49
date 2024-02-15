5bf832660b8af076140e2559efe1fd5f7404ecde
Albert-back-end <153019657+Albert-back-end@users.noreply.github.com>
2024-02-15 15:20:33 +0300

    add files brain_even.py and engine.py for the brain_even game logic

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,12 @@
 from prompt import string
 
-
+input_name = None
 def welcome_user():
+    global input_name
     print("Welcome to the Brain Games!")
     input_name = string("May I have your name? ")
     print(f"Hello, {input_name}!")
+
+
+def return_input_name():
+    return input_name
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from brain_games.engine import logic_of_game
+
+
+def main():
+    logic_of_game()
+
+
+if __name__ == "main":
+    main()
